eb97c4cf957de878ee0cabbb581e0c36f9363e68
Sungmann Cho <sungmann.cho@gmail.com>
2024-10-23 08:08:27 +0900

    Remove env_logger and log from the dependencies list

--- src/lib.rs
+++ src/lib.rs
@@ -57,18 +57,15 @@ impl ffi::breakpad::ExceptionHandlerDelegate_methods for ExceptionHandlerDelegat
     }
 }
 
+#[allow(unused_variables)]
 pub trait ExceptionHandlerDelegate {
-    fn did_write_minidump(&self, working_path: String, minidump_id: String) {
-        log::debug!("[did_write_minidump] working_path={working_path} minidump_id={minidump_id}");
-    }
+    fn did_write_minidump(&self, working_path: String, minidump_id: String) {}
 
     fn get_working_path(&self) -> String {
-        log::debug!("[get_working_path] return=empty string");
         String::new()
     }
 
     fn should_write_minidump(&self) -> bool {
-        log::debug!("[should_write_minidump] return=true");
         true
     }
 }
